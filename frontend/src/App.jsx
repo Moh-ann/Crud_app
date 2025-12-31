abf3f691c83d3a184028e624825fc6056d2a9de1
MohanK-pixel <mohnk007@gmail.com>
2025-12-31 12:49:49 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+      <h1 className="text-red-600 font-bold underline">Hello, World!</h1>
+      <button className='btn btn-primary'>click</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
